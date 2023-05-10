3a533cbc4e3cc2026f41646e1e28b5a23e80f3f0
Benoit Favier <benoitfamillefavier@gmail.com>
2023-05-10 22:34:24 +0200

    draft

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import streamlit as st
 import pygmalion as ml
-import re
-import string
+from skimage.transform import resize
+import numpy as np
 from typing import List
-from itertools import zip_longest
 from skimage.io import imread
+
+classes = ['road', 'ground', 'sky', 'building', 'vegetation', 'vehicle', 'human', 'obstacle', 'trafic sign', 'other']
+colors = np.array([[200, 200, 200], [150, 80, 0], [130, 180, 255], [100, 100, 100], [0, 150, 0], [250, 100, 0], [255, 0, 0], [180, 0, 180], [255, 255, 0], [0, 0, 0]], dtype=np.uint8)
 
 
 @st.cache_resource(show_spinner=False)
 def download_model():
-    return ml.load_model("https://drive.google.com/file/d/1l4jxLJpLt8xmxf9JMIVM4lYv42erF--0/view?usp=share_link")
+    return ml.load_model("https://drive.google.com/file/d/1s2GXAY34ScvZyFJL099GlwUcTM3dhyz8/view?usp=share_link")
 
 
 @st.cache_resource(show_spinner=False)
@@ -17,22 +19,26 @@ def get_picture():
     return imread("picture.png")
 
 
-def capitalize(string: str) -> str:
-    return string[0].upper() + string[1:]
+@st.cache_resource(show_spinner=False)
+def get_default_image():
+    return imread("place.jpg")
 
 
-def format_sentences(text: str) -> List[str]:
+@st.cache_resource(show_spinner=False)
+def resized(image):
     """
-    Split an input text into formated input sentences
+    Return the image resized and croped to a power of two dimension
     """
-    pattern = "(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!)\s+"
-    sentences = re.split(pattern, text)
-    spacers = re.findall(pattern, text)
-    sentences = (capitalize(s).strip() for s in sentences)
-    sentences = (s.replace(" ,", ",").replace(" .", ".") for s in sentences)
-    sentences = (re.sub("(?<=[^\\s\\^]):", " :", re.sub("(?<=[^\\s\\^])\?", " ?", re.sub("(?<=[^\\s\\^])!", " !", s))) for s in sentences)
-    sentences = [s if s.endswith((".", "?", "!", ":")) else s+"." for s in sentences]
-    return sentences, spacers
+    h, w = image.shape[:2]
+    H, W = (2**np.floor([np.log2(h), np.log2(w)])).astype(int)
+    i, j = (h - H)//2, (w - W)//2
+    image = image[i:i+H, j:j+W, ...]
+    image = resize(image, (256, 512), anti_aliasing=True)
+    if np.all(0 <= image) and np.all(image <= 1):
+        image = image * 255
+    image = image.astype("uint8")
+    return image
+
 
 with st.sidebar:
     st.image(get_picture())
@@ -42,29 +48,34 @@ with st.sidebar:
     st.markdown("[Ma page GitHub](https://github.com/BFavier)")
     st.markdown("[Ma page Linkedin](https://www.linkedin.com/in/benoit-favier-9694b9206/)")
 
-st.title("Traduction français → anglais")
-st.markdown("Cette application est une démonstration d'un modèle de NMT (Neural Machine Translation). "
-            "La traduction est effectuée par un modèle de machine learning (Transformer) "
-            "entraîné sur ~3 millions de paires de phrases français/anglais. "
-            "Le modèle est appliqué phrase par phrase, sans tenir compte du contexte du document entier.")
-st.markdown("Vous pouvez par exemple le tester avec des paragraphes issus d'une page aléatoire de [Wikipedia](https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard).")
-st.subheader("Le texte à traduire:")
-input_text = st.text_area(label="Le texte à traduire:",
-                          placeholder="Pinocchio avait toujours voulu devenir un véritable petit garçon...",
-                          max_chars=10000, height=200, label_visibility="collapsed")
+st.title("Road images segmentation")
+st.markdown(".")
+image = resized(st.camera_input("Take a picture") or get_default_image())
+
 
 with st.spinner(text="Téléchargement du modèle ..."):
     model = download_model()
 
-if len(input_text) > 0:
-    inputs, spacers = format_sentences(input_text)
-    predictions = []
-    progress_bar = st.progress(0., "Application du modèle ...")
-    for i, input in enumerate(inputs, start=1):
-        predictions.append(model.predict(input, n_beams=3)[0])
-        progress_bar.progress(int(i*100/len(inputs)), "Traduction en cours ...")
-    predictions = ["".join(filter(lambda x: x in string.printable, s)) for s in predictions]  # Filtre les charactères non-affichables
-    progress_bar.empty()
-    st.caption("Traduction:")
-    translation = "".join(s for t in zip_longest(predictions, spacers, fillvalue="") for s in t)
-    st.markdown(translation)
+
+@st.cache_resource(show_spinner=False)
+def colored(image: np.ndarray) -> np.ndarray:
+    prediction = model.predict(image[None, ...])[0]
+    dy, dx = np.diff(prediction, n=1, axis=0), np.diff(prediction, n=1, axis=1)
+    boundaries = (np.pad(dy, ((1, 0), (0, 0))) | np.pad(dy, ((1, 0), (0, 0))) | np.pad(dx, ((0, 0), (1, 0))) | np.pad(dx, ((0, 0), (0, 1))))
+    target_colors = colors[prediction]
+    opacity = np.where(boundaries, np.array([[1.0]]), np.array([[0.5]]))[..., None]
+    result = image.astype(float) * (1 - opacity) + opacity * target_colors.astype(float)
+    result = np.clip(np.round(result, 0), 0, 255).astype("uint8")
+    return result
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.write('')
+
+with col2:
+    st.image(colored(image))
+
+with col3:
+    st.write('')
+
