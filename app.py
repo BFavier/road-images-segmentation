6c1f39171430889942a50441b211bc65596b94e1
Benoit Favier <benoitfamillefavier@gmail.com>
2023-05-11 11:36:29 +0200

    changed description

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ with st.sidebar:
     st.markdown("[Ma page Linkedin](https://www.linkedin.com/in/benoit-favier-9694b9206/)")
 
 st.title("Road images segmentation")
-st.markdown("This is a semantic segmentation model trained on dashcams in urban environments. It segments the image into ten classes.")
+st.markdown("This is a semantic segmentation model trained on dashcams in urban environments. It segments the image into ten classes. The image must be in landscape format.")
 source = st.radio("Get image from", ('default', 'camera', 'upload'), horizontal=True)
 if source == "camera":
     image_input = st.camera_input("Take a picture", label_visibility="collapsed")
